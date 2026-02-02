adac01d7d74fc5bb5bf897ca50f8075e699f7ad9
newby-by <practikum10@gmail.com>
2026-02-02 19:11:43 +0300

    Add test with add a book with a unique name

--- test_books_collector.py
+++ test_books_collector.py
@@ -12,3 +12,14 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         assert len(collector.get_books_rating()) == 2
+
+    def test_add_new_book_with_unique_name(self):
+        """Add a book with a unique name."""
+        collector = BooksCollector()
+        name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(name)
+        expected_books_genre = collector.get_books_genre()
+        
+        collector.add_new_book(name)
+
+        assert collector.get_books_genre() == expected_books_genre
