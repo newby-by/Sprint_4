44ffc37911b7941eb7906584ecbaf08205b650b4
newby-by <practikum10@gmail.com>
2026-02-06 20:04:36 +0300

    Fix delete useless adding genre

--- test_books_collector.py
+++ test_books_collector.py
@@ -135,47 +135,41 @@ class TestBooksCollector:
         """Add a unique name of a book that is in books_genre."""
         collector = BooksCollector()
         expected_favorites_length = len(collector.favorites)
-        expected_favorite_movie_name = 'ОНО'
+        expected_favorite_book_name = 'ОНО'
         collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
-        collector.add_new_book(expected_favorite_movie_name)
-        collector.set_book_genre(expected_favorite_movie_name, 'Комедии')
+        collector.add_new_book(expected_favorite_book_name)
 
-        collector.add_book_in_favorites(expected_favorite_movie_name)
+        collector.add_book_in_favorites(expected_favorite_book_name)
 
         assert (len(collector.get_list_of_favorites_books()) ==
                 expected_favorites_length + 1 and
-                expected_favorite_movie_name in
+                expected_favorite_book_name in
                 collector.get_list_of_favorites_books())
 
     def test_delete_book_from_favorites_with_exist_name(self):
         """Remove an exist name of a book that is in favorites."""
         collector = BooksCollector()
-        expected_favorite_movie_name = 'ОНО'
-        collector.add_new_book('Гордость и предубеждение и зомби',)
-        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
-        collector.add_new_book(expected_favorite_movie_name)
-        collector.set_book_genre(expected_favorite_movie_name, 'Комедии')
-        collector.add_book_in_favorites(expected_favorite_movie_name)
+        expected_favorite_book_name = 'ОНО'
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book(expected_favorite_book_name)
+        collector.add_book_in_favorites(expected_favorite_book_name)
         expected_favorites_length = len(collector.favorites)
 
-        collector.delete_book_from_favorites(expected_favorite_movie_name)
+        collector.delete_book_from_favorites(expected_favorite_book_name)
 
         assert (len(collector.get_list_of_favorites_books()) ==
                 expected_favorites_length - 1 and
-                expected_favorite_movie_name not in
+                expected_favorite_book_name not in
                 collector.get_list_of_favorites_books())
 
     def test_get_list_of_favorites_books_with_unempty_favorites(self):
         """Show a unempty list of favorites that is not empty."""
         collector = BooksCollector()
-        expected_favorite_movie_name = 'ОНО'
+        expected_favorite_book_name = 'ОНО'
         collector.add_new_book('Гордость и предубеждение и зомби',)
-        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
-        collector.add_new_book(expected_favorite_movie_name)
-        collector.set_book_genre(expected_favorite_movie_name, 'Комедии')
-        collector.add_book_in_favorites(expected_favorite_movie_name)
+        collector.add_new_book(expected_favorite_book_name)
+        collector.add_book_in_favorites(expected_favorite_book_name)
 
         actual_favorites = collector.get_list_of_favorites_books()
         assert (len(collector.favorites) == 1 and
-                actual_favorites == [expected_favorite_movie_name])
+                actual_favorites == [expected_favorite_book_name])
